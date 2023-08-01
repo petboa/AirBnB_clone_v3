cf3c884bf6ce7e84cc340fb21aebe644c220ff72
Peter Boakye <petboa123@gmail.com>
2023-08-01 16:49:50 +0000

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -2,14 +2,11 @@
 """
 Contains class BaseModel
 """
-
-from datetime import datetime
 import models
-from os import getenv
-import sqlalchemy
+import uuid
+from datetime import datetime
 from sqlalchemy import Column, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
-import uuid
 
 time = "%Y-%m-%dT%H:%M:%S.%f"
 
@@ -58,7 +55,7 @@ class BaseModel:
         models.storage.new(self)
         models.storage.save()
 
-    def to_dict(self):
+    def to_dict(self, save_pass=False):
         """returns a dictionary containing all keys/values of the instance"""
         new_dict = self.__dict__.copy()
         if "created_at" in new_dict:
@@ -68,6 +65,11 @@ class BaseModel:
         new_dict["__class__"] = self.__class__.__name__
         if "_sa_instance_state" in new_dict:
             del new_dict["_sa_instance_state"]
+        if '_password' in new_dict:
+            new_dict['password'] = new_dict['_password']
+            new_dict.pop('_password', None)
+        if not save_pass:
+            new_dict.pop('password', None)
         return new_dict
 
     def delete(self):
